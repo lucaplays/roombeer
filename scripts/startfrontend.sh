478f66cb118a4c416d2881d82bbae421d12cf8f4
Lukas Aichbauer <lukas.aichbauer@wolfvision.net>
2025-05-23 23:57:33 +0200

    - Waiting in frontend script unless ip is obtained.

--- scripts/startfrontend.sh
+++ scripts/startfrontend.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-IP=$(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+IP=""
+while [ -z $IP ]; do
+    IP=$(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+    sleep 1
+done
+
 cd ../room-beer-vue/
 npm install
 echo "Running on $IP"
