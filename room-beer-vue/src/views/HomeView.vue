2ef63fb8b0765aeadeba054356c53231120b497f
Lukas Aichbauer <lukas.aichbauer@wolfvision.net>
2025-05-23 23:53:22 +0200

    - Added debug ip message to frontend script.

--- room-beer-vue/src/views/HomeView.vue
+++ room-beer-vue/src/views/HomeView.vue
@@ -2,8 +2,10 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>ROOMBEER!</h1>
+    <h2>{{ IP }}</h2>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { IP } from '@/constants'
 </script>
