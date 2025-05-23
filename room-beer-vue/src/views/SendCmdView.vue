1a8ceff22c0e946ce7b17e4c86c967d1a91db52a
Arda Yalcin <arda.yalcin@wolfvision.net>
2025-05-23 16:46:52 +0200

    - setup frontend backend communcation template

---
+++ room-beer-vue/src/views/SendCmdView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click="sendCommand">Send Command</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import axios from 'axios';
+
+const sendCommand = async () => {
+  const command = {
+    action: 'some_action',
+    data: {
+      key: 'value'
+    }
+  };
+
+  try {
+    const response = await axios.post('http://localhost:8000/execute-command/', command);
+    console.log(response.data);
+  } catch (error) {
+    console.error('Error sending command:', error);
+  }
+};
+</script>
--- venv_backend/src/main.py
+++ venv_backend/src/main.py
@@ -3,8 +3,17 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Item(BaseModel):
     name: str
