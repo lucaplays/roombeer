e474c1b5bbd2a56166ab2a973782eebb3675da68
Lukas Aichbauer <lukas.aichbauer@wolfvision.net>
2025-05-24 00:44:05 +0200

    - Increased resolution and framerate.

--- scripts/startstream.sh
+++ scripts/startstream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Thanks to https://github.com/jacksonliam/mjpg-streamer
-./mjpg_streamer/mjpg_streamer -i "./mjpg_streamer/input_uvc.so -d /dev/video0 -r 640x480 -f 10" -o "./mjpg_streamer/output_http.so -p 1234 -w ./www"
+./mjpg_streamer/mjpg_streamer -i "./mjpg_streamer/input_uvc.so -d /dev/video0 -r 1920x1080 -f 15" -o "./mjpg_streamer/output_http.so -p 1234 -w ./www"
