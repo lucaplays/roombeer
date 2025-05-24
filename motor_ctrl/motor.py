c53d6680b57e14ecc77a90f7dd6051fb0f8049c0
Johannes Marte <johannes.marte@wolfvision.net>
2025-05-24 09:50:55 +0200

    test

--- motor_ctrl/motor.py
+++ motor_ctrl/motor.py
@@ -11,13 +11,15 @@ class controller:
 
     def __init__(self, device: str):
         self.device = device
-        self.fd = open(device, "rw")
+        self.fd = os.open(device, os.O_RDWR)
         self.speed_left = 0
         self.speed_right = 0
 
         # self.__push_speed()
 
     def __push_int16(buffer: list[int], speed: int):
+        print("push buffer")
+        print(speed)
         buffer.append(speed & 0xFF)
         buffer.append((speed & 0xFF00) >> 8)
 
@@ -30,9 +32,8 @@ class controller:
         controller.__push_int16(buffer, self.speed_right)
         controller.__push_int16(buffer, self.speed_left)
 
-        self.fd.write(bytes(buffer))
         print(bytes(buffer))
-        self.fd.flush()
+        os.write(self.fd, bytes(buffer))
 
     def set_motor_speed(self, left_speed_float: float, right_speed_float: float):
         self.speed_left = int(left_speed_float * 4000)
