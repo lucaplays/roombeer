58dbea482d40b7576ef3b8dfd6400f8c989122af
lucaplays <55000584+lucaplays@users.noreply.github.com>
2025-05-24 03:52:03 +0200

    test langsame

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ class MoveItem(BaseModel):
 
 @app.post("/move/")
 def move(move_item: MoveItem):
-    on_off = int(move_item.is_down);
+    on_off = int(move_item.is_down) * 0.1;
     
     match move_item.direction:
         case Direction.FORWARD:
